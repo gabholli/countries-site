b32606b73f1c13ac556553a6763c7bb7a8c7a7cf
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2025-03-11 16:19:06 -0700

    Added styling to home page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,11 +2,11 @@ import { Link } from "react-router";
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center p-2 gap-y-4 text-2xl">
+    <div className="flex flex-col justify-center items-center p-4 m-2 gap-y-4 text-2xl bg-neutral-100 rounded-4xl">
       <h1 className="text-center md:w-80">Welcome to REST Countries, a site where you can find out about your favorite country</h1>
       <Link
         to="countries"
-        className="border-2 rounded-full py-2 px-4 bg-blue-400 text-white"
+        className="border-2 rounded-full py-2 px-4 bg-blue-400 text-white flex text-center"
       >Find out about a country
       </Link>
     </div>
