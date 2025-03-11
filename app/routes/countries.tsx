6d23d03495281452957227588c0dc965a72a8884
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2025-03-11 16:09:39 -0700

    Added search input field

--- app/routes/countries.tsx
+++ app/routes/countries.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link, useLoaderData, type LoaderFunctionArgs } from "react-router";
 
 export async function clientLoader({ request }: LoaderFunctionArgs) {
@@ -8,9 +9,16 @@ export async function clientLoader({ request }: LoaderFunctionArgs) {
 }
 
 export default function Countries() {
+    const [search, setSearch] = useState("")
     const data = useLoaderData()
 
-    const sortedData = data?.sort((a: any, b: any) => {
+    const filteredCountries = data?.filter((country: any) => {
+        const matchesSearch =
+            !search || country.name.common.toLowerCase().includes(search.toLowerCase())
+        return matchesSearch
+    })
+
+    const sortedData = filteredCountries?.sort((a: any, b: any) => {
         return a.name.common.localeCompare(b.name.common)
     })
 
@@ -19,7 +27,7 @@ export default function Countries() {
             <Link
                 key={country.name.common}
                 to="#"
-                className="text-normal hover:underline flex flex-col justify-center items-center
+                className="text-normal flex flex-col justify-center items-center
                     bg-neutral-100 px-2 py-4 rounded-3xl shadow-xl"
             >
                 <p className="font-bold">{country.name.common}</p>
@@ -33,6 +41,14 @@ export default function Countries() {
 
     return (
         <div className="flex flex-col justify-center items-center gap-y-8 mt-8 mb-8">
+            <input
+                className="px-2 py-4 indent-4 rounded-2xl border-black border-2"
+                placeholder="Enter country here..."
+                type="text"
+                value={search}
+                onChange={(e) => { setSearch(e.target.value) }}
+            >
+            </input>
             <h1 className="font-extrabold text-2xl">List of countries:</h1>
             <main className="flex flex-col justify-center items-center gap-y-8
                 md:grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-items-center">
