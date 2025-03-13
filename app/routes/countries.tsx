807346d7541d4bece3ba04280f0e5483af65c5ba
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2025-03-13 14:27:05 -0700

    Added pagination

--- app/routes/countries.tsx
+++ app/routes/countries.tsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, useLoaderData, type LoaderFunctionArgs } from "react-router";
 import BackToTop from "~/components/backToTop";
 
 export async function clientLoader({ request }: LoaderFunctionArgs) {
     const response = await fetch("https://restcountries.com/v3.1/all")
-    const data = response.json()
+    const data = await response.json()
     console.log(data)
     return data
 }
@@ -12,12 +12,18 @@ export async function clientLoader({ request }: LoaderFunctionArgs) {
 export default function Countries() {
     const [search, setSearch] = useState("")
     const [region, setRegion] = useState("")
+    const [currentPage, setCurrentPage] = useState(1)
+    const countriesPerPage = 10 // Number of countries per page
+
     const data = useLoaderData()
+
+    useEffect(() => {
+        setCurrentPage(1)
+    }, [search, region])
 
     const filteredCountries = data?.filter((country: any) => {
         const matchesRegion = !region || country.region.toLowerCase().includes(region.toLowerCase())
-        const matchesSearch =
-            !search || country.name.common.toLowerCase().includes(search.toLowerCase())
+        const matchesSearch = !search || country.name.common.toLowerCase().includes(search.toLowerCase())
         return matchesSearch && matchesRegion
     })
 
@@ -25,7 +31,12 @@ export default function Countries() {
         return a.name.common.localeCompare(b.name.common)
     })
 
-    const listOfCountries = sortedData?.map((country: any) => {
+    // Pagination logic
+    const indexOfLastCountry = currentPage * countriesPerPage
+    const indexOfFirstCountry = indexOfLastCountry - countriesPerPage
+    const currentCountries = sortedData?.slice(indexOfFirstCountry, indexOfLastCountry)
+
+    const listOfCountries = currentCountries?.map((country: any) => {
         return (
             <Link
                 key={country.name.common}
@@ -40,7 +51,13 @@ export default function Countries() {
         )
     })
 
-    console.log(listOfCountries)
+    // Change page
+    const paginate = (pageNumber: number) => setCurrentPage(pageNumber)
+
+    const pageNumbers = []
+    for (let i = 1; i <= Math.ceil(sortedData?.length / countriesPerPage); i++) {
+        pageNumbers.push(i)
+    }
 
     return (
         <div className="flex flex-col justify-center items-center gap-y-8 mt-8 mb-8">
@@ -63,18 +80,31 @@ export default function Countries() {
                     <option value="africa">Africa</option>
                     <option value="asia">Asia</option>
                     <option value="oceania">Oceania</option>
-
                 </select>
             </div>
             <h1 className="font-extrabold text-2xl text-white">List of countries:</h1>
             {listOfCountries.length > 0 ? (
                 <main className="flex flex-col justify-center items-center gap-8
-                 md:grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-items-center">
+                    md:grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-items-center">
                     {listOfCountries}
                 </main>
             ) :
                 <h1 className="text-white font-bold text-xl">No results...</h1>
             }
+            <nav className="w-74 bg-neutral-100 px-2 py-4 rounded-2xl">
+                <ul className="flex flex-wrap justify-center items-center gap-4">
+                    {pageNumbers.map(number => (
+                        <li key={number} className="">
+                            <button
+                                onClick={() => paginate(number)}
+                                className="cursor-pointer w-4 hover:underline"
+                            >
+                                {number}
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
             <BackToTop />
         </div>
     )
