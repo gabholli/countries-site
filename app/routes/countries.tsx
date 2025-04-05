7f331751b2d4726eb41a513f5026663155fd0a5a
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2025-04-04 17:58:10 -0700

    Updated local storage variable for null values

--- app/routes/countries.tsx
+++ app/routes/countries.tsx
@@ -10,7 +10,7 @@ export async function clientLoader({ request }: LoaderFunctionArgs) {
 }
 
 export default function Countries() {
-    const searchItemFromLocalStorage = JSON.parse(localStorage.getItem("searchItem"))
+    const searchItemFromLocalStorage = JSON.parse(localStorage.getItem("searchItem") || "")
     const [search, setSearch] = useState("")
     const [region, setRegion] = useState("")
     const [currentPage, setCurrentPage] = useState(1)
